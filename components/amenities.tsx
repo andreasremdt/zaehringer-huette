a0fb9c623b647b9a1248ca14f52196576d675951
Andreas Remdt <me@andreasremdt.com>
2024-08-28 17:49:44 +0200

    feat: re-design amenities

--- components/amenities.tsx
+++ components/amenities.tsx
@@ -1,9 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import Wifi from "@/public/images/wifi.svg";
-import Kitchen from "@/public/images/kitchen.svg";
-import People from "@/public/images/people.svg";
-import Fireplace from "@/public/images/fireplace.svg";
 import Icon from "@/components/icon";
 
 export default function Amenities() {
@@ -14,103 +10,161 @@ export default function Amenities() {
           Ausstattung
         </h2>
 
-        <ul className="grid grid-cols-2 gap-8 text-center font-serif text-lg font-medium md:grid-cols-4 md:gap-16 md:text-xl">
-          <li>
-            <Image
-              src={Wifi}
-              width={64}
-              height={64}
-              alt=""
-              className="mx-auto mb-4 size-12 md:size-16"
-              aria-hidden="true"
-            />
-            WLAN mit 50 mbit/s
-          </li>
-          <li>
-            <Image
-              src={Kitchen}
-              width={64}
-              height={64}
-              alt=""
-              className="mx-auto mb-4 size-12 md:size-16"
-              aria-hidden="true"
-            />
-            Moderne Küche mit Vollausstattung
-          </li>
-          <li>
-            <Image
-              src={People}
-              width={64}
-              height={64}
-              alt=""
-              className="mx-auto mb-4 size-12 md:size-16"
-              aria-hidden="true"
-            />
-            Platz für bis zu 11 Personen
-          </li>
-          <li>
-            <Image
-              src={Fireplace}
-              width={64}
-              height={64}
-              alt=""
-              className="mx-auto mb-4 size-12 md:size-16"
-              aria-hidden="true"
-            />
-            2 Kachelöfen in Wohn- und Esszimmer
-          </li>
-        </ul>
-
-        <ul className="mt-16 flex flex-col justify-center gap-8 text-center font-serif text-lg font-medium md:flex-row md:text-xl">
-          <li className="md:w-2/4 lg:w-1/3">
-            <strong className="mb-2 block text-xl md:text-2xl">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          <article>
+            <h3 className="mb-4 font-serif text-2xl md:mb-8 md:text-3xl lg:text-4xl">
+              Allgemeines
+            </h3>
+            <ul className="space-y-4">
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/wifi.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                WLAN mit 50 mbit/s
+              </li>
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/kitchen.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                Moderne Küche mit Vollausstattung
+              </li>
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/people.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                Platz für bis zu 11 Personen
+              </li>
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/fireplace.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                2 Kachelöfen in Wohn- und Esszimmer
+              </li>
+            </ul>
+          </article>
+          <article>
+            <h3 className="mb-4 font-serif text-2xl md:mb-8 md:text-3xl lg:text-4xl">
               Übernachtung
-            </strong>
-            <ul>
-              <li className="flex justify-center gap-2">
-                <Icon name="minus" className="mt-2 size-3" /> Platz für 4 - 11
-                Personen
+            </h3>
+            <ul className="space-y-4">
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/bed.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                2 Doppelzimmer
               </li>
-              <li className="flex justify-center gap-2">
-                <Icon name="minus" className="mt-2 size-3" /> 2 Doppelzimmer
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/double-bedroom.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                1 Zweibettzimmer
               </li>
-              <li className="flex justify-center gap-2">
-                <Icon name="minus" className="mt-2 size-3" /> 1 Zweibettzimmer
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/bunk-bed.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                1 Dreibettzimmer
               </li>
-              <li className="flex justify-center gap-2">
-                <Icon name="minus" className="mt-2 size-3" /> 1 Dreibettzimmer
-              </li>
-              <li className="flex justify-center gap-2">
-                <Icon name="minus" className="mt-2 size-3" /> 1 Zimmer mit
-                Schlafcouch
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/couch.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                1 Zimmer mit Schlafcouch
               </li>
             </ul>
-          </li>
-          <li className="md:w-2/4 lg:w-1/3">
-            <strong className="mb-2 block text-xl md:text-2xl">
+          </article>
+          <article>
+            <h3 className="mb-4 font-serif text-2xl md:mb-8 md:text-3xl lg:text-4xl">
               Sanitäre Anlagen
-            </strong>
-            <ul>
-              <li className="flex justify-center gap-2">
-                <Icon name="minus" className="mt-2 size-3" />
-                Dusche und WC sind neu renoviert
-                <br />
-                (behindertengerecht) ebenerdig
+            </h3>
+            <ul className="space-y-4">
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/shower.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                Behindertengerechte Duschen und WCs
               </li>
-              <li className="flex justify-center gap-2">
-                <Icon name="minus" className="mt-2 size-3" /> Separate
-                Waschbecken pro Zimmer
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/sink.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                Separate Waschbecken pro Zimmer
               </li>
-              <li className="flex justify-center gap-2">
-                <Icon name="minus" className="mt-2 size-3" /> 2 Toiletten
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/toilet.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                2 Toiletten
               </li>
-              <li className="flex justify-center gap-2">
-                <Icon name="minus" className="mt-2 size-3" /> Warmwasser überall
-                im Haus
+              <li className="flex items-center gap-4">
+                <Image
+                  src="/images/warm-water.svg"
+                  width={32}
+                  height={32}
+                  alt=""
+                  className="size-8"
+                  aria-hidden="true"
+                />
+                Warmwasser überall im Haus
               </li>
             </ul>
-          </li>
-        </ul>
+          </article>
+        </div>
 
         <Link
           href="/buchen"
