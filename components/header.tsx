cd38b9eceb02a5482b038b8374a7a4bf9affbe29
Andreas Remdt <me@andreasremdt.com>
2024-09-29 11:55:18 +0200

    fix: hover colors in header

--- components/header.tsx
+++ components/header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
           <li>
             <a
               href="tel:+491739940283"
-              className="inline-flex items-center gap-2 py-2 text-xs font-medium uppercase transition-transform duration-300 ease-in-out sm:text-sm md:px-0"
+              className="inline-flex items-center gap-2 py-2 text-xs font-medium uppercase transition-colors ease-in-out hover:text-primary-200 sm:text-sm md:px-0"
             >
               <Icon name="phone" className="size-4 shrink-0" />
               +49 (0)173 9940 283
@@ -25,7 +25,7 @@ export default function Header() {
           <li>
             <a
               href="mailto:info@zaehringer-huette.de"
-              className="inline-flex items-center gap-2 py-2 text-xs font-medium uppercase transition-transform duration-300 ease-in-out sm:text-sm md:px-0"
+              className="inline-flex items-center gap-2 py-2 text-xs font-medium uppercase transition-colors ease-in-out hover:text-primary-200 sm:text-sm md:px-0"
             >
               <Icon name="envelope" className="size-4 shrink-0" />
               Kontakt
@@ -54,7 +54,7 @@ export default function Header() {
 
           <nav
             className={cn(
-              "fixed bottom-0 right-0 flex flex-col bg-secondary-950 px-4 py-4 transition-all duration-300 ease-in-out md:visible md:static md:top-0 md:w-full md:translate-x-0 md:flex-row md:items-center md:justify-center md:gap-12 md:border-none md:bg-transparent md:px-0",
+              "fixed bottom-0 right-0 flex flex-col bg-secondary-950 px-4 py-4 transition-all ease-in-out md:visible md:static md:top-0 md:w-full md:translate-x-0 md:flex-row md:items-center md:justify-center md:gap-12 md:border-none md:bg-transparent md:px-0",
               {
                 "invisible translate-x-full": !open,
                 "top-16": scrolled,
@@ -66,31 +66,31 @@ export default function Header() {
           >
             <Link
               href="/#die-huette"
-              className="px-8 py-2 text-sm font-medium uppercase transition-transform duration-300 ease-in-out md:px-0"
+              className="px-8 py-2 text-sm font-medium uppercase transition-colors ease-in-out hover:text-primary-200 md:px-0"
             >
               Die Hütte
             </Link>
             <Link
               href="/#die-umgebung"
-              className="px-8 py-2 text-sm font-medium uppercase transition-transform duration-300 ease-in-out md:px-0"
+              className="px-8 py-2 text-sm font-medium uppercase transition-colors ease-in-out hover:text-primary-200 md:px-0"
             >
               Die Umgebung
             </Link>
             <Link
               href="/#galerie"
-              className="px-8 py-2 text-sm font-medium uppercase transition-transform duration-300 ease-in-out md:px-0"
+              className="px-8 py-2 text-sm font-medium uppercase transition-colors ease-in-out hover:text-primary-200 md:px-0"
             >
               Galerie
             </Link>
             <Link
               href="/buchen#preise"
-              className="px-8 py-2 text-sm font-medium uppercase transition-transform duration-300 ease-in-out md:px-0"
+              className="px-8 py-2 text-sm font-medium uppercase transition-colors ease-in-out hover:text-primary-200 md:px-0"
             >
               Preise
             </Link>
             <Link
               href="/buchen"
-              className="px-8 py-2 text-sm font-medium uppercase transition-transform duration-300 ease-in-out md:px-0"
+              className="px-8 py-2 text-sm font-medium uppercase transition-colors ease-in-out hover:text-primary-200 md:px-0"
             >
               Buchen
             </Link>
