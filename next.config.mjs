85fa0823ad75edaac117bddc0d1ed1255496d420
Andreas Remdt <me@andreasremdt.com>
2025-01-09 17:41:27 +0100

    feat: add react compiler

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [NEXT_PUBLIC_SERVER_URL].map((item) => {
